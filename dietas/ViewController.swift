f0d953352769d28b835331e28bec30781d984c8f
Ronald Cruz <ronaldcruzcano@gmail.com>
2018-03-18 21:58:04 -0500

    Initial Commit

---
+++ dietas/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dietas
+//
+//  Created by Ronald Cruz Cano on 18/03/18.
+//  Copyright © 2018 Ronald Cruz Cano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
